5e81478380c88ba3e0e86b9aa03406249fbf65d2
bysong <bysong@tudou.com>
2015-03-30 15:54:56 +0800

    add displaymetric output.

--- app/alt_resource/src/com/example/apklauncher_app_alt_res/App.java
+++ app/alt_resource/src/com/example/apklauncher_app_alt_res/App.java
@@ -1,7 +1,10 @@
 package com.example.apklauncher_app_alt_res;
 
 import android.app.Application;
+import android.hardware.display.DisplayManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
 public class App extends Application {
 	private static final String TAG = App.class.getSimpleName();
@@ -11,6 +14,9 @@ public class App extends Application {
 		super.onCreate();
 		
 		Log.d(TAG, "congfig: " + getResources().getConfiguration());
+		DisplayMetrics dm = new DisplayMetrics();
+		((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getMetrics(dm);
+		Log.d(TAG, "dm: " + dm);
 		
 		int dimen = getResources().getDimensionPixelSize(R.dimen.alt_dimen);
 		Log.d(TAG, "dimen: " + dimen);
