a62ba1577c547c31debc8fd45c232be3ab0ab2f7
bysong <bysong@tudou.com>
2015-04-09 11:54:12 +0800

    use export api.

--- app/alt_resource/src/com/example/apklauncher_app_alt_res/MainActivity.java
+++ app/alt_resource/src/com/example/apklauncher_app_alt_res/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.apklauncher_app_alt_res;
 
-import org.bbs.apklauncher.emb.auto_gen.Target_Activity;
+import org.bbs.apklauncher.api.Base_Activity;
 
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends Target_Activity {
+public class MainActivity extends Base_Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
 	
 	@Override
