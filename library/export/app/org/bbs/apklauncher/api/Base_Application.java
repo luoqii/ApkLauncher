d65e5064087a6bd1f5ce133a6929ce4e390e0e98
bysong.androidstudio <bysong@tudou.com>
2015-06-09 14:56:25 +0800

    add Base_Application api.

---
+++ library/export/app/org/bbs/apklauncher/api/Base_Application.java
@@ -0,0 +1,7 @@
+package org.bbs.apklauncher.api;
+
+import android.app.Application;
+
+public class Base_Application extends Application {
+
+}
---
+++ library/export/plugin/org/bbs/apklauncher/api/Base_Application.java
@@ -0,0 +1,7 @@
+package org.bbs.apklauncher.api;
+
+import org.bbs.apklauncher.emb.Target_Application;
+
+public class Base_Application extends Target_Application {
+
+}
